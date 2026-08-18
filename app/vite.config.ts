7a0a1a96a24133be414e367c6f3b86524baf049d
River <me@river.berlin>
2026-08-18 20:46:50 +0200

    Configure Vite proxy to seamlessly serve docs on localhost:5173/docs/

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,4 +3,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    proxy: {
+      '/docs': {
+        target: 'http://localhost:4321',
+        changeOrigin: true,
+      },
+      '/_astro': {
+        target: 'http://localhost:4321',
+        changeOrigin: true,
+      },
+    },
+  },
 });
